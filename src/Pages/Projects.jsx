61752a7cfb0a45aa5c71c42dde705f944c94518a
Reignz217 <renzharveysunico17@gmail.com>
2025-05-04 23:09:52 +0800

    5/4/25 11:05PM

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -6,7 +6,7 @@ export default function Projects(){
         <>
         <h5 className="lg:text-5xl md:text-3xl text-xl font-bold text-center mb-5 text-[#00ADB5]">Projects I've Worked On</h5>
         <div className="lg:grid grid-cols-2 gap-5 md:px-20 px-5 text-center">
-            <div>
+            <div className='show'>
                 <img src={Proj1} className="hover:scale-103 transition-all duration-300 ease-in-out mb-5"/>
                 <h5 className="md:text-2xl text-xl font-bold">Health Information System</h5>
                 <p className='font-medium tracking-wider text-sm'>( This is our Capstone in college )</p>
@@ -15,7 +15,7 @@ export default function Projects(){
                                     
                 </p>
                 <div className='text-left py-5 md:grid grid-cols-2'>
-                    <div  className='p-3'>
+                    <div  className='p-3 show'>
                         <h5 className='font-bold text-xl'>My Role:</h5>
                         <ul className='list-disc px-10 text-sm md:text-lg'>
                             <li>Assisted in Front-End Development</li>
@@ -23,7 +23,7 @@ export default function Projects(){
                             <li>Assisted in UI/UX development</li>
                         </ul>
                     </div>
-                    <div className='p-3'>
+                    <div className='p-3 show'>
                         <h5 className='font-bold text-xl'>Tools:</h5>
                         <ul className='list-disc px-10 text-sm md:text-lg'>
                             <li>HTML</li>
@@ -35,7 +35,7 @@ export default function Projects(){
                     </div>
                 </div>
             </div>
-            <div>
+            <div className='show'>
                 <img src={Proj2} className="hover:scale-103 transition-all duration-300 ease-in-out mb-5"/>
                 <h5 className="md:text-2xl text-xl font-bold">iReply HRIS Payroll System</h5>
                 <p className='font-medium tracking-wider text-sm'>( This is our work in Internship Program )</p>
@@ -44,7 +44,7 @@ export default function Projects(){
                                     payroll system ensures employees are paid accurately and on time while complying with government regulations and labor laws. 
                                     It can also keep records of attendance, leaves, and employee benefits.
                 </p>
-                <div className='text-left py-5 md:grid grid-cols-2'>
+                <div className='text-left py-5 md:grid grid-cols-2 show'>
                     <div className='p-3'>
                         <h5 className='font-bold text-xl'>My Role:</h5>
                         <ul className='list-disc px-10 text-sm md:text-lg'>
